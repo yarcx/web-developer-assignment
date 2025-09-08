489f0c10b6b5a0fa196d7bd17ab4701b72d8b15c
yarcx <yakubuh56@gmail.com>
2025-09-08 13:38:09 +0100

    Updated the origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", [
     "localhost",
     "https://presley-unbehaving-ollie.ngrok-free.app",
+    "https://web-developer-assignment-omega.vercel.app/",
     "*",
   ]);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
